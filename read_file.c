58b7d41e9fa51ecc599b13da758bd030688d1d48
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-16 03:34:57 -0800

    modify read_file

--- read_file.c
+++ read_file.c
@@ -1,20 +1,18 @@
 #include "monty.h"
 
 /**
- * read_file - reads a file
- * @fd: pointer to file descriptor
+ * read_file - reads a file.
+ * @fd: file descriptor.
+ *
  * Return: void
  */
-
 void read_file(FILE *fd)
 {
-	int line_number, format = 0;
+	int ln, format = 0;
 	char *buffer = NULL;
 	size_t len = 0;
 
-	for (line_number = 1; getline(&buffer, &len, fd) != -1; line_number++)
-	{
-		format = parse_line(buffer, line_number, format);
-	}
+	for (ln = 1; getline(&buffer, &len, fd) != -1; ln++)
+		format = parse_line(buffer, ln, format);
 	free(buffer);
 }
