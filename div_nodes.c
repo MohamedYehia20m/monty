d885b34849238c9dc77cf83c8dfb2bdebb4ac4e3
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:32:57 +0200

    fix div_nodes bugs

--- div_nodes.c
+++ div_nodes.c
@@ -3,20 +3,20 @@
 /**
  * div_nodes - Adds the top two elements of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @ln: Interger representing the line number of of the opcode.
  */
-void div_nodes(stack_t **stack, unsigned int line_number)
+void div_nodes(stack_t **stack, unsigned int ln)
 {
-	int sum;
+	int submission;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "div");
+		more_err(8, ln, "div");
 
 	if ((*stack)->n == 0)
-		more_err(9, line_number);
+		more_err(9, ln);
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n / (*stack)->prev->n;
-	(*stack)->n = sum;
+	submission = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = submission;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
