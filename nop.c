fdcc89ebf63e6d72fd99b4a5a8a6f25f874f37b3
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:55:03 +0200

    fix nop bugs

--- nop.c
+++ nop.c
@@ -3,10 +3,10 @@
 /**
  * nop - Does nothing.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @ln: Interger representing the line number of of the opcode.
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int ln)
 {
 	(void)stack;
-	(void)line_number;
+	(void)ln;
 }
