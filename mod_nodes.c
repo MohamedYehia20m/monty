8b506bf5d65ec2c954d355546f42cd519c981986
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:49:31 +0200

    fix mod_nodes bugs

--- mod_nodes.c
+++ mod_nodes.c
@@ -3,19 +3,19 @@
 /**
  * mod_nodes - Adds the top two elements of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @ln: Interger representing the line number of of the opcode.
  */
-void mod_nodes(stack_t **stack, unsigned int line_number)
+void mod_nodes(stack_t **stack, unsigned int ln)
 {
 	int sum;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
-		more_err(8, line_number, "mod");
+		more_err(8, ln, "mod");
 
 
 	if ((*stack)->n == 0)
-		more_err(9, line_number);
+		more_err(9, ln);
 	(*stack) = (*stack)->next;
 	sum = (*stack)->n % (*stack)->prev->n;
 	(*stack)->n = sum;
