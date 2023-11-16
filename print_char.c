1bd703c2f415b7a947bf820fe48eeb2c07e9f828
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-16 03:55:30 -0800

    modify print_char

--- print_char.c
+++ print_char.c
@@ -2,18 +2,17 @@
 
 /**
  * print_char - Prints the Ascii value.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @st: Pointer to a pointer pointing to top of the stack.
+ * @line_number: line number of of the opcode.
  */
-void print_char(stack_t **stack, unsigned int line_number)
+void print_char(stack_t **st, unsigned int line_number)
 {
 	int ascii;
 
-	if (stack == NULL || *stack == NULL)
-		string_err(11, line_number);
-
-	ascii = (*stack)->n;
+	if (st == NULL || *st == NULL)
+		errors_10_11(11, line_number);
+	ascii = (*st)->n;
 	if (ascii < 0 || ascii > 127)
-		string_err(10, line_number);
+		errors_10_11(10, line_number);
 	printf("%c\n", ascii);
 }
