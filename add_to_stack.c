07e8fa364df2400af0afc8fc789689749434cdf5
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:16:55 +0200

    fixing add_to_stack bugs

--- add_to_stack.c
+++ add_to_stack.c
@@ -2,23 +2,23 @@
 
 
 /**
- * add_to_stack - Adds a node to the stack.
- * @new_node: Pointer to the new node.
+ * push - Adds a node to the stack.
+ * @new_element: Pointer to the new node.
  * @ln: Interger representing the line number of of the opcode.
  */
-void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+void push(stack_t **new_element, __attribute__((unused))unsigned int ln)
 {
-	stack_t *tmp;
+	stack_t *temp;
 
-	if (new_node == NULL || *new_node == NULL)
+	if (new_element == NULL || *new_element == NULL)
 		exit(EXIT_FAILURE);
 	if (head == NULL)
 	{
 		head = *new_node;
 		return;
 	}
-	tmp = head;
-	head = *new_node;
-	head->next = tmp;
-	tmp->prev = head;
+	temp = head;
+	head = *new_element;
+	head->next = temp;
+	temp->prev = head;
 }
