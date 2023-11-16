154c0e61b8dc9022bdb3356934d1f3957df7e7f8
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-16 03:27:05 -0800

    modify swap_nodes

--- swap_nodes.c
+++ swap_nodes.c
@@ -2,21 +2,21 @@
 
 /**
  * swap_nodes - Swaps the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @st: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
-void swap_nodes(stack_t **stack, unsigned int line_number)
+void swap_nodes(stack_t **st, unsigned int line_number)
 {
 	stack_t *tmp;
 
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "swap");
-	tmp = (*stack)->next;
-	(*stack)->next = tmp->next;
+	if (st == NULL || *st == NULL || (*st)->next == NULL)
+		errors_6_9(8, line_number, "swap");
+	tmp = (*st)->next;
+	(*st)->next = tmp->next;
 	if (tmp->next != NULL)
-		tmp->next->prev = *stack;
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
+		tmp->next->prev = *st;
+	tmp->next = *st;
+	(*st)->prev = tmp;
 	tmp->prev = NULL;
-	*stack = tmp;
+	*st = tmp;
 }
