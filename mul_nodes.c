86dcef4d2a146a70200a348032b1061fe79a6590
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:52:27 +0200

    fix mul_nodes bugs

--- mul_nodes.c
+++ mul_nodes.c
@@ -3,14 +3,14 @@
 /**
  * mul_nodes - Adds the top two elements of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @ln: Interger representing the line number of of the opcode.
  */
-void mul_nodes(stack_t **stack, unsigned int line_number)
+void mul_nodes(stack_t **stack, unsigned int ln)
 {
 	int sum;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "mul");
+		more_err(8, ln, "mul");
 
 	(*stack) = (*stack)->next;
 	sum = (*stack)->n * (*stack)->prev->n;
