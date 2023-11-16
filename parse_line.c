a724d46cb87df3acb958143ccdc410e9e93beb0c
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:59:28 +0200

    fix parse_line bugs

--- parse_line.c
+++ parse_line.c
@@ -3,22 +3,22 @@
 /**
  * parse_line - Separates each line into tokens to determine
  * which function to call
- * @buffer: line from the file
- * @line_number: line number
+ * @bf: line from the file
+ * @ln: line number
  * @format:  storage format. If 0 Nodes will be entered as a stack.
  * if 1 nodes will be entered as a queue.
  * Return: Returns 0 if the opcode is stack. 1 if queue.
  */
 
-int parse_line(char *buffer, int line_number, int format)
+int parse_line(char *bf, int ln, int format)
 {
 	char *opcode, *value;
 	const char *delim = "\n ";
 
-	if (buffer == NULL)
+	if (bf == NULL)
 		err(4);
 
-	opcode = strtok(buffer, delim);
+	opcode = strtok(bf, delim);
 	if (opcode == NULL)
 		return (format);
 	value = strtok(NULL, delim);
@@ -28,6 +28,6 @@ int parse_line(char *buffer, int line_number, int format)
 	if (strcmp(opcode, "queue") == 0)
 		return (1);
 
-	find_func(opcode, value, line_number, format);
+	find_func(opcode, value, ln, format);
 	return (format);
 }
