9f66875cebee6a2a8dab1fc7725bd5e9b07dc2c1
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-16 13:21:26 +0200

    fixing add_nodes bugs

--- add_nodes.c
+++ add_nodes.c
@@ -3,14 +3,14 @@
 /**
  * add_nodes - Adds the top two elements of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @ln: Interger representing the line number of of the opcode.
  */
-void add_nodes(stack_t **stack, unsigned int line_number)
+void add_nodes(stack_t **stack, unsigned int ln)
 {
 	int sum;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "add");
+		more_err(8, ln, "add");
 
 	(*stack) = (*stack)->next;
 	sum = (*stack)->n + (*stack)->prev->n;
